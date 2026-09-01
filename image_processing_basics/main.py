9c4e78d9a93cf02d19f988a9f9b92098189f18d2
Kauser <nazkauser413@gmail.com>
2026-09-01 16:31:04 +0500

    Refactored

---
+++ image_processing_basics/main.py
@@ -0,0 +1,10 @@
+from images import img
+import cv2
+
+# Reads the image as a NumPy array
+def read_image(img):
+    return
+    cv2.imshow('Image Window', img)
+    cv2.waitKey(0)
+
+print(read_image("input.jpg"))
